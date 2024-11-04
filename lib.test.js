910d8bb6bc5a2570bd6c79fc4fd92f72942105ed
choehyeonjin <guswls2616@naver.com>
2024-11-04 23:38:58 +0900

    Feat: test for prime function

--- lib.test.js
+++ lib.test.js
@@ -8,3 +8,15 @@ test("avg([3, 5, 7]) should be 5", () => {
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
 });
+
+test("prime(-1) should be false", () => {
+    expect(lib.prime(-1)).toBe(false);
+});
+
+test("prime(10) should be false", () => {
+    expect(lib.prime(10)).toBe(false);
+});
+
+test("prime(2) should be true", () => {
+    expect(lib.prime(2)).toBe(true);
+});
